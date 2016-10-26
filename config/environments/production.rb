259851464248c0136b0475988774acca56270b79
Matt Oberle <moberle@nypublicradio.org>
2016-10-26 15:35:18 -0400

    sets relative_url_root for reverse proxy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Enables operation behind reverse proxy
+  config.action_controller.relative_url_root = "/transcribe"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
